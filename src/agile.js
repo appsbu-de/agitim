87efff8018fb22335f9891c2a0637d8420358089
Carsten Sandtner <mail@casa-rock.de>
2014-04-28 21:31:47 +0200

    Added new frontend and class.

---
+++ src/agile.js
@@ -0,0 +1,39 @@
+(function() {
+
+    var timer = new Timer(),
+        numParticipants,
+        meetingDuration;
+
+    function Agile() {
+        numParticipants = 1;
+        meetingDuration = 30;
+    }
+
+    Agile.prototype.setNumParticipants = function(participants) {
+        console.log(participants);
+        numParticipants = participants || 1;
+    };
+
+    Agile.prototype.getParticipants = function() {
+        return numParticipants;
+    };
+
+    Agile.prototype.getDuration = function() {
+        return meetingDuration;
+    };
+
+    Agile.prototype.setDuration = function(duration) {
+        meetingDuration = duration || 30;
+    };
+
+    Agile.prototype.calculateSpeakingTime = function() {
+        if (numParticipants < 1 || meetingDuration < 1) {
+            return 0;
+        }
+
+        return (meetingDuration/numParticipants);
+    };
+
+    window.Agile = Agile;
+
+})();
