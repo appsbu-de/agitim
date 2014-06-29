18535cbfe67c6de6ca8cd967dbf7e4b3ef20d985
Carsten Sandtner <mail@casa-rock.de>
2014-06-29 14:23:40 +0200

    Added flipbox and separated Settings and countdown.

--- src/app.js
+++ src/app.js
@@ -3,18 +3,32 @@ function initTimer() {
     var timer = document.getElementById('timerPerAttendee'),
         timerTotal = document.getElementById('timerTotal'),
         agile = new Agile(),
+        isCounting = false,
         sbDuration = document.getElementById('duration'),
         sbParticipants = document.getElementById('participants'),
         actionButton = document.getElementById('action'),
-        viewParticipantsElement = document.getElementById('numParticipants');
+        viewParticipantsElement = document.getElementById('numParticipants'),
+        counterFlipbox = document.getElementById('counterBox'),
+        cancelTimerButton = document.getElementById('cancelTimer');
 
     sbDuration.onchange = sbParticipants.onchange = function() {
-        setTimer();
+        if (!isCounting) {
+            setTimer();
+        }
+    };
+
+    cancelTimerButton.onclick = function() {
+        timer.stop();
+        timerTotal.stop();
+        counterFlipbox.toggle();
+        isCounting = false;
     };
 
     actionButton.onclick = function() {
+        counterFlipbox.toggle();
         timer.start();
         timerTotal.start();
+        isCounting = true;
     };
 
     function setTimer() {
